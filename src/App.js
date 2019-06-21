e9742adc287c35a538306c75705993cd0c707a4d
Artem Dorokhov <artdor1978@gmail.com>
2019-06-21 12:28:45 +0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
+function parseHtmlEnteties(str) {
+    return str.replace(/&#([0-9]{1,4});/gi, function(match, numStr) {
+        var num = parseInt(numStr, 10); // read num as normal number
+        return String.fromCharCode(num);
+    });
+}
+
+class App extends Component {
+
+constructor(props) {
+  super(props);
+  this.state = {
+    //loading: false,
+    quote: {
+      content: '',
+      title: ''}
+  };
+  this.getQuote = this.getQuote.bind(this)
+}
+
+componentDidMount() {
+  this.getQuote();
+}
+
+getQuote () {
+  const i = Math.floor(Math.random() * (1125 + 1))
+fetch('http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&_='+i)
+  .then(response => {
+    if(response.ok) return response.json();
+      throw new Error('Request failed.');
+})
+  .then(data => {
+    this.setState({
+      quote: data[0]
+  });
+  })
+.catch(error => {
+      console.log(error);
+    });
+}
+
+render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div id="quote-box">
+      <div id = "text">{'"' + parseHtmlEnteties(this.state.quote.content.replace(/<\/?[^>]+>/gi, ''))}</div>
+      <div id = "author">{"- " + this.state.quote.title}</div>
+      <a id="tweet-quote" target="_blank" title="Tweet this quote!"
+      href={'http://twitter.com/intent/tweet?&text='+'"' + parseHtmlEnteties(this.state.quote.content.replace(/<\/?[^>]+>/gi, ''))+' '+this.state.quote.title} rel="noopener noreferrer">
+      <i class="fa fa-twitter"></i></a>
+      <button class="btn" id = "new-quote" onClick={this.getQuote}>New Quote</button>
+     </div>
+ )
+}
 }
 
 export default App;
