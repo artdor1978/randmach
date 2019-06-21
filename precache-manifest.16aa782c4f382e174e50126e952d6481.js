6d7488691cb29528f45c8a09222f8c4e95dc8e17
Artem Dorokhov <artdor1978@gmail.com>
2019-06-21 12:49:05 +0300

    Updates

---
+++ precache-manifest.16aa782c4f382e174e50126e952d6481.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7077ab2127899984a66f141d16af6658",
+    "url": "/randmach/index.html"
+  },
+  {
+    "revision": "7a77fe4008cf3c8ed734",
+    "url": "/randmach/static/css/main.2bc31b79.chunk.css"
+  },
+  {
+    "revision": "eb5d03ae7c9a30028659",
+    "url": "/randmach/static/js/2.4e81ba5b.chunk.js"
+  },
+  {
+    "revision": "7a77fe4008cf3c8ed734",
+    "url": "/randmach/static/js/main.af0268c6.chunk.js"
+  },
+  {
+    "revision": "0aa17af5d46d1b8da815",
+    "url": "/randmach/static/js/runtime~main.e816a61a.js"
+  }
+]);
